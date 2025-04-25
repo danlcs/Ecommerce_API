135cd3773f488f6ce0e327234de12c5baee72e6c
danlcs <danielsilva123lucas@gmail.com>
2025-04-25 10:56:24 -0400

    new file:   app.py 	new file:   requirements.txt

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# Definir uma rota raiz (pág. inicial) e a função que será executada ao requisitar
+@app.route('/teste')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
