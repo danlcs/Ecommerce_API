35d9392ffe977bca5336869de6f0fa6a40b2890f
danlcs <danielsilva123lucas@gmail.com>
2025-04-25 15:19:30 -0400

    Added routes to the main page and add product page

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
 
-# Definir uma rota raiz (pág. inicial) e a função que será executada ao requisitar
-@app.route('/teste')
+db = SQLAlchemy(app)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    
+
+@app.route('/api/products/add', methods=['POST']) # Definir rota para página de adição de produtos e função que a executa
+def add_product():
+    if 'name' in data and 'price' in data:
+        data = request.json
+        product = Product(name=data['name'], price=data['price'], description=data.get('description', 'ERRO'))
+        db.session.add(product)
+        db.session.commit()
+        return jsonify({'message':'Product added successfully'}), 200
+    return jsonify({'message': 'Invalid data, missing price or name'}), 400
+
+@app.route('/') # Definir uma rota raiz (pág. inicial) e a função que será executada ao requisitar
 def hello_world():
     return 'Hello, World!'
 
